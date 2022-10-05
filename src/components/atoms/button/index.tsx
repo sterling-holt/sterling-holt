716f11ab0c05b8d5745946f19130da6bcd2e606e
Sterling <jsterlingholt@me.com>
2022-10-05 15:43:16 -0500

    setting up foundation to create a link between JSX props and SCSS

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,49 +1,18 @@
-import React, { ReactElement } from 'react'
+import React from 'react'
+
 import style from './button.module.scss'
 
-//  ----------------
 
-export const Button = (props: { 
-    children: ReactElement | string,
-    onClick: Function,
-    className: any,
-    forwardRef: any
+export const Button = (props: {
+    children: any,
 }) => {
 
-    const [ mouse, setMouse ] = React.useState([]) as any
-    const ButtonRef = React.useRef()
-
-    function MouseMove(x: any, y: any) {
-        setMouse([ x, y ])
-    }
-    
-    
-    React.useEffect(() => {
-        console.log(mouse)
-        console.log(ButtonRef)
-    }, [ mouse ])
-    
-    
-    React.useEffect(() => {
-        
-        props.forwardRef.current !== 'undefined ' ?
-            console.log(props.forwardRef.current.setAttribute('style', 'background: "blue"'))
-            :null
-
-    }, [props.forwardRef])
 
 
-    //  --------
     return <button
-        ref={props.forwardRef}
-        className={`${style.component} ${props.className}`}
-        onMouseMove={(e) => {
-            MouseMove(e.clientX, e.clientY)
-        }}
-        onMouseDown={() => {}}
-        onClick={(e) => {
-            props.onClick(e)
-        }}>
-        { props.children }
+        className={style.btn}
+        
+        {...props}>
+        Button text
     </button>
 }
