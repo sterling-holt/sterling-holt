beddc27440cd15f75e798b2c85851d1d9a5db3c6
Sterling <jsterlingholt@me.com>
2022-10-17 00:57:40 -0500

    updating api interaction

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-
-const nextConfig = {
-}
-
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-
+  
   webpack: ( config ) => {
     config.resolve.fallback = { fs: false };
     
