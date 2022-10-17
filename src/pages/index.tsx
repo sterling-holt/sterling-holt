b84083a92cc7d949d587471b5d6c418479156bd8
Sterling <jsterlingholt@me.com>
2022-10-17 00:42:17 -0500

    renaming env references

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
             method: "get",
             mode: "cors",
             headers: { 
-                authorization: `token ${process.env.GITHUB_AUTH}`
+                authorization: `token ${process.env.GH_AUTH}`
             }
         })
 
