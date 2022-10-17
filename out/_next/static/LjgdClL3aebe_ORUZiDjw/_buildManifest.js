704aec5ea49b01358ff722282cb5a02a5c59110b
Sterling <jsterlingholt@me.com>
2022-10-17 01:15:03 -0500

    static export

--- out/_next/static/LjgdClL3aebe_ORUZiDjw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5db05b0601a78ea9.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/project/[id]":["static/chunks/pages/project/[id]-1cd0db3db9d1169b.js"],sortedPages:["/","/_app","/_error","/project/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
